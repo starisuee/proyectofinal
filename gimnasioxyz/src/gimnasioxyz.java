847866868ef326980cd23c484439643c2eb481d8
alvaradooct1 <85977170+alvaradooct1@users.noreply.github.com>
2022-07-10 11:26:52 -0500

    Update gimnasioxyz.java
    Menú mejorado un poco, correcciones a llamadas de métodos

--- gimnasioxyz/src/gimnasioxyz.java
+++ gimnasioxyz/src/gimnasioxyz.java
@@ -1,3 +1,4 @@
+package proyectofinal;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -80,91 +81,106 @@ public class gimnasioxyz {
     }
 
     public int menu() {
-        int i = 0, si_no = 0; //esto se reemplaza por un caracter 's' o 'n'
+        int i = 0;
+        String si_no = ""; //esto se reemplaza por un caracter 's' o 'n'
+        int j = 0;
         int opc = 0;
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.println("-----CATALOGO-----");
-            System.out.println("(1) Administrar instructores ");
-            System.out.println("(2) Administrar clientes ");
-            System.out.println("(3) Administrar planes de servicio ");//solo cambiar los precios, puede sr un arreglo de flotantes con los precios
+            System.out.println("1. Catálogo(Mantenimiento - crear, modificar, eliminar)");
             System.out.println("--------------------------------");
-            System.out.println("(4) Registro de ingreso por mes"); // los precios deben de ser fijos
+            System.out.println("2. Registro de ingreso por mes"); // los precios deben de ser fijos
             System.out.println("--------------------------------");
-            System.out.println("Reportes o consultas");
-            System.out.println("(5) Listado de clientes por instructor");
-            System.out.println("(6) Listado de ingresos por mes");
-            System.out.println("(7) Listar el total (dinero) de las suscripciones por mes.");
-            System.out.println("(8) Listar el promedio de ingresos para los tres meses");
-            System.out.println("(9) Listar los meses en los que el ingreso fue mayor al promedio");
-            System.out.println("(10) Listar los meses en los que el ingreso fue menor al promedio");
+            System.out.println("3. Reportes o consultas");
+            System.out.println("- Listado de clientes por instructor");
+            System.out.println("- Listado de ingresos por mes");
+            System.out.println("- Listar el total (dinero) de las suscripciones por mes.");
+            System.out.println("- Listar el promedio de ingresos para los tres meses");
+            System.out.println("- Listar los meses en los que el ingreso fue mayor al promedio");
+            System.out.println("- Listar los meses en los que el ingreso fue menor al promedio"); //Aún no los quito del menú principal porque no están hechos
             System.out.println("--------------------------------");
-            System.out.println("(11) Salir");
+            System.out.println("4. Salir");
 
 
-            System.out.println("introduzca la opcion deseada (1-8)");
+            System.out.println("Introduzca la opcion deseada (1-4)");
             opc = sc.nextInt();
             switch (opc) {
                 case 1:
-                    administrar_instructor();
+                    System.out.println("");
+                    System.out.println("Seleccione su siguiente opción:");
+                    System.out.println("1. Administrar instructores ");
+                    System.out.println("2. Administrar planes de subscripción ");
+                    System.out.println("3. Administrar clientes");//solo cambiar los precios, puede sr un arreglo de flotantes con los precios
+                    j = sc.nextInt();
+                    switch(j) {
+                    case 1:
+                    	administrar_instructor();
+                    	break;
+                    case 2:
+                    	administrar_planes();
+                    	break;
+                    case 3:
+                    	administrar_cliente();
+                    	break;
+                    default:
+                    	System.out.println("Opción inválida");
+                    	break;
+                    }
                     menu();
                     break;
                 case 2:
-                    administrar_cliente();
                     menu();
                     break;
                 case 3:
-                    administrar_planes();
                     menu();
                     break;
                 case 4:
+                	System.out.println("¿Está seguro que desea salir? S/N");
+                	si_no = sc.nextLine();
+                	switch(si_no) {
+                	case "s":
+                	case "S":
+                		System.exit(0);
+                		break;
+                	case "n":
+                	case "N":
+                		menu();
+                	}
                     break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    System.exit(0);
-                    break;
+                default:
+                	System.out.println("Opción inválida");
+                	break;
             }
-            System.out.println("Desea continuar en el sistema? S/N");
-        } while (i < 11);
+        } while (i < 4);
 
         return opc;
     }
 
     public int administrar_instructor() {
         int opc = 0;
-        String[] nombre = new String[5];
+        String[] nombre = new String[3];
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("");
         System.out.println("////Administrar entrenador////");
-        System.out.println("(1) Agregar entrenadores");
-        System.out.println("(2) Modificar entrenadores");
-        System.out.println("(3) Eliminar entrenadores");
-        System.out.println("Introduzca la opcion deseada (1-3)");
+        System.out.println("1. Agregar entrenadores");
+        System.out.println("2. Modificar entrenadores");
+        System.out.println("3. Eliminar entrenadores");
+        System.out.println("Introduzca la opción deseada (1-3)");
         opc = sc.nextInt();
         switch (opc) {
             case 1:
-                System.out.println("Crear entrenador?");
-                ingresar_instructor();
+                System.out.println("Crear entrenador");
+                ingresar_instructor(); // aqui se guarda un arreglo con 3 nombres de instructores
                 menu();
                 break;
             case 2:
-                System.out.println("modificar entrenador");
+                System.out.println("Modificar entrenador");
                 modificar_instructor();
                 menu();
                 break;
             case 3:
-                System.out.println("eliminar entrenador");
+                System.out.println("Eliminar entrenador");
                 eliminar_instructor();
                 menu();
         }
@@ -173,37 +189,65 @@ public class gimnasioxyz {
 
     public int administrar_cliente() {
         int opc = 0;
-        String[] nombre = new String[3];
+        String[] nombre = new String[5];
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("");
         System.out.println("////Administrar perfiles////");
-        System.out.println("(1) Ingresar cliente");
-        System.out.println("(2) Agregar entrenadores");
-        System.out.println("Modificar entrenadores");
-        System.out.println("Eliminar entrenadores");
+        System.out.println("1. Ingresar cliente");
+        System.out.println("2. Agregar entrenadores");
+        System.out.println("3. Modificar entrenadores");
+        System.out.println("4. Eliminar entrenadores");
         System.out.println("Introduzca la opcion deseada (1-4)");
         opc = sc.nextInt();
         switch (opc) {
             case 1:
-                System.out.println("Crear entrenador?");
-                ingresar_instructor(); // aqui se guarda un arreglo con 3 nombres de instructores
+                System.out.println("Crear cliente");
+                ingresar_cliente();
                 menu();
                 break;
             case 2:
-                System.out.println("modificar entrenador");
-                modificar_instructor();
+                System.out.println("Modificar cliente");
+                modificar_cliente();
                 menu();
                 break;
             case 3:
-                System.out.println("eliminar entrenador");
-                eliminar_instructor();
+                System.out.println("Eliminar cliente");
+                eliminar_cliente();
                 menu();
         }
         return opc;
     }
 
-    public void administrar_planes() {
-
+    public int administrar_planes() {
+    	int opc = 0;
+    	String[] plan = new String[3];
+    	
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("");
+    	System.out.println("////Administrar planes////");
+    	System.out.println("1. Crear planes");
+        System.out.println("2. Modificar planes");
+        System.out.println("3. Eliminar planes");
+        System.out.println("Introduzca la opcion deseada (1-3)");
+        opc = sc.nextInt();
+        switch (opc) {
+            case 1:
+                System.out.println("Crear planes");
+                crear_plan();
+                menu();
+                break;
+            case 2:
+                System.out.println("Modificar planes");
+                modificar_instructor();
+                menu();
+                break;
+            case 3:
+                System.out.println("Eliminar planes");
+                eliminar_instructor();
+                menu();
+        }
+        return opc;
     }
 
     public String[] ingresar_instructor() {
@@ -230,7 +274,7 @@ public class gimnasioxyz {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("que instructor desea modificar?");
+        System.out.println("¿Qué instructor desea modificar?");
         for (i = 0; i < modificado.length; i++) {
             System.out.println("instructor " + modificado[i]);
         }
@@ -241,12 +285,12 @@ public class gimnasioxyz {
                 System.out.println("Escriba el nuevo nombre: ");
                 nombre_n = sc.next();
                 modificado[i] = nombre_n;
-                System.out.println("Se actualizo el nombre correctamente");
+                System.out.println("Se actualizó el nombre correctamente");
 
             }
         }
         for (i = 0; i < modificado.length; i++) {
-            System.out.println("instructor " + modificado[i]);
+            System.out.println("Instructor " + modificado[i]);
         }
 
         return modificado;
@@ -259,9 +303,9 @@ public class gimnasioxyz {
         String elim = "";
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("que instructor desea eliminar?");
+        System.out.println("¿Qué instructor desea eliminar?");
         for (i = 0; i < eliminado.length; i++) {
-            System.out.println("instructor " + eliminado[i]);
+            System.out.println("Instructor " + eliminado[i]);
         }
         System.out.println("Escriba el nombre del instructor: ");
         elim = sc.next();
@@ -269,12 +313,12 @@ public class gimnasioxyz {
             if (eliminado[i].equalsIgnoreCase(elim)) {
 
                 eliminado[i] = "null";
-                System.out.println("Se elimino el nombre correctamente");
+                System.out.println("Se eliminó el nombre correctamente");
 
             }
         }
         for (i = 0; i < eliminado.length; i++) {
-            System.out.println("instructor " + eliminado[i]);
+            System.out.println("Instructor " + eliminado[i]);
         }
 
         return eliminado;
@@ -293,12 +337,12 @@ public class gimnasioxyz {
             nombre[i] = nombres;
         }
         for (i=0; i<idc.length; i++){
-            System.out.println("ingrese la id del cliente");
+            System.out.println("Ingrese la id del cliente");
             id = sc.nextInt();
             ids[i] = id;
         }
         for (i=0;i<tel.length; i++){
-            System.out.println("Ingrese el numero de telefono del cliente");
+            System.out.println("Ingrese el número de teléfono del cliente");
             tele = sc.nextInt();
             telf[i] = tele;
         }
@@ -320,7 +364,7 @@ public class gimnasioxyz {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("que cliente desea modificar?");
+        System.out.println("¿Qué cliente desea modificar?");
         for (i = 0; i < modificado.length; i++) {
             System.out.println("instructor " + modificado[i]);
         }// aqui muestro el contenido del arreglo para que se ingrese cual se desea modificar
@@ -331,12 +375,12 @@ public class gimnasioxyz {
                 System.out.println("Escriba el nuevo nombre: ");
                 nombre_n = sc.next();
                 modificado[i] = nombre_n;
-                System.out.println("Se actualizo el nombre correctamente");
+                System.out.println("Se actualizó el nombre correctamente");
 
             }
         }
         for (i = 0; i < modificado.length; i++) {
-            System.out.println("instructor " + modificado[i]);
+            System.out.println("Instructor " + modificado[i]);
         }
 
         return modificado;
@@ -349,29 +393,35 @@ public class gimnasioxyz {
         String elim = "";
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("que instructor desea eliminar?");
+        System.out.println("Qué instructor desea eliminar?");
         for (i = 0; i < eliminado.length; i++) {
-            System.out.println("instructor " + eliminado[i]);
+            System.out.println("Instructor " + eliminado[i]);
         }// aqui muestro el contenido del arreglo para que se ingrese cual se desea eliminar
         System.out.println("Escriba el nombre del instructor: ");
         elim = sc.next();
         if (eliminado[i].equalsIgnoreCase(elim)) {// aqui se utiliza un metodo que compara el dato a eliminar para poder actualizarlo
 
             eliminado[i] = "null";
-            System.out.println("Se elimino el cliente correctamente");
+            System.out.println("Se eliminó el cliente correctamente");
 
         }
         for (i = 0; i < eliminado.length; i++) {
-            System.out.println("cliente " + eliminado[i]);
+            System.out.println("Cliente " + eliminado[i]);
         }
 
         return eliminado;
     }
-
-
+    public String[] crear_plan() {
+    	String[] planes = new String[3];
+    	return planes;
+    }
+    public String[] modificar_plan() {
+    	String[] modificar = new String[3];
+    	return modificar;
+    }
+    public String[] eliminar_plan() {
+    	String[] eliminar = new String[3];
+    	return eliminar;
+    }
+    //Voy a ver los métodos de los planes, están sin hacer para que pueda subir la clase main de una
 }
-
-
-
-
-
