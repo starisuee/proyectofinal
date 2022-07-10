3205c0fbcc57bcdc56ddbde8b055bd6fe4190249
alvaradooct1 <85977170+alvaradooct1@users.noreply.github.com>
2022-07-10 11:27:16 -0500

    Add files via upload

---
+++ gimnasioxyz/src/ProyectoFinalMain.java
@@ -0,0 +1,12 @@
+package proyectofinal;
+import java.util.Scanner;
+public class ProyectoFinalMain {
+
+	public static void main(String[] args) {
+		int opcion = 0;
+		Scanner sc = new Scanner(System.in);
+		gimnasioxyz gym = new gimnasioxyz();
+		System.out.println("Bienvenido al programa");
+	    gym.menu();
+	}
+}
